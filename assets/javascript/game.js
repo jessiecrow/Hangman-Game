8b9b247802aa2df5ab566ceb6478fe35da5c6bb3
Jessie Crow <jrmcrow@gmail.com>
2017-05-15 19:09:08 -0400

    major progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,34 @@
 var
-	usedLetters = [abcdefghijklmnopqrstuvwxyz];
-	answer;
+	usedHeroes = ["genji", "mccree", "pharah", "reaper", "soldier", "sombra", "tracer", "bastion", "hanzo", "junkrat", "mei", "torbjorn", "widowmaker", "dva", "orisa", "reinhardt", "roadhog", "winston", "zarya", "ana", "lucio", "mercy", "symmetra", "zenyatta"];
+	hero = "";
+	blanks = [];
+	userGuesses = "";
+	message = "";
+	guessesLeft = 0;
+	lettersLeft = 0;
 
+function randomWord(){
+	hero = usedHeroes[Math.floor(Math.random() * usedHeroes.length)];
+	return;
+}
+function setBlanks(){
+	blanks = [];
+	for (var i = 0; i < word.length; i++) {
+		blanks = " _ ";
+	}
+	document.getElementById("blanks").innerHTML = blanks.join(" ");
+	return;
+}
+function setBoard(){
+	guessesLeft = 10;
+	document.getElementById("remaining").innerHTML = "Lives Remaining: " + guessesLeft;
+}
+document.getElementById("start").onclick = function() {
+	randomWord();
+	setBlanks();
+	setBoard();
+	console.log(word);
+}
 document.onkeyup = function(event) {
 	var inputKey = String.fromCharCode(event.keyCode).toLowerCase();
 	for (var i = 0; i < ---.length; i++) {
